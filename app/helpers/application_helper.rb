6cca4c6774eda58a3a1cfcc83d28c545b897ad3c
theverything <j3ffhorn@gmail.com>
2013-03-19 17:55:47 -0700

    add unique hash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def unique_hash_creator
+    uniq_array = ('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a
+    hash = []
+    5.times { hash << uniq_array[rand(0..60)] }
+    hash.join
+  end
+  
 end
