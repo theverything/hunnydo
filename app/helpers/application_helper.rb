7afad472a7aed7430fba5c4f94eb821239f3e246
theverything <j3ffhorn@gmail.com>
2013-03-26 21:24:42 -0700

    use securerandom for hash generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,8 @@
+require 'securerandom'
 module ApplicationHelper
 
   def unique_hash_creator
-    uniq_array = ('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a
-    hash = []
-    5.times { hash << uniq_array[rand(0..60)] }
-    hash.join
+    SecureRandom.urlsafe_base64(4)
   end
   
 end
